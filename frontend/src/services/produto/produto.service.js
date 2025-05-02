eaa731a07b3437a48293a0728edd1d1d828e771e
ROBBZZ <bynho92k@gmail.com>
2025-05-01 22:26:29 -0300

    :sparkles: Feat - criando serviço dos produtos

---
+++ frontend/src/services/produto/produto.service.js
@@ -0,0 +1,13 @@
+import { api } from "@/api";
+
+class ProdutoService {
+  async getProdutos(){
+    return await api.get("/get.php")
+  }
+
+
+}
+
+const produtoService = new ProdutoService();
+
+export { produtoService };
