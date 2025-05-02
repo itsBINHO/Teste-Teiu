ca9aca52d1e896b8ca8be5c583b0e75ad73a6040
ROBBZZ <bynho92k@gmail.com>
2025-05-01 23:17:07 -0300

    :recycle:	Refactor - Refatorando estrutura de código

--- backend/conexao.php
+++ backend/conexao.php
@@ -1,14 +1,14 @@
 <?php
 
-    $hostname = "localhost";
-    $username = "root";
-    $password = "Limitbreaker2020!";
-    $database = "dbproducts";
+$hostname = "localhost";
+$username = "root";
+$password = "Limitbreaker2020!";
+$database = "dbproducts";
 
-    // Cria a conexão com o banco de dados e a verifica
-    $conn = new mysqli($hostname, $username, $password, $database);
+$conn = new mysqli($hostname, $username, $password, $database);
 
-    if ($conn->connect_error) {
-        die("A Conexão falhou: " . $conn->connect_error);
-    } 
+if ($conn->connect_error) {
+    die("A Conexão falhou: " . $conn->connect_error);
+}
+
 ?>
